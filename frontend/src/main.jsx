38a3a87772ff056b5279eb06b44b98f9cd20b6ca
Muhammad Anus <anasshahid161@gmail.com>
2025-11-30 00:46:21 +0500

    Fix: Remove react-helmet-async from main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,19 +4,16 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import { CartProvider } from './context/CartContext.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
-import { HelmetProvider } from 'react-helmet-async';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <AuthProvider>
-        <CartProvider>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-        </CartProvider>
-      </AuthProvider>
-    </HelmetProvider>
+    <AuthProvider>
+      <CartProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </CartProvider>
+    </AuthProvider>
   </React.StrictMode>,
 )
